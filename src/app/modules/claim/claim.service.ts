05742f3467bd218827e79f3e2b09df94b9671fae
Md. Mehedi Hasan Nabil <mehedihasannabil49@gmail.com>
2024-04-02 14:13:02 +0600

    Get Claims

--- src/app/modules/claim/claim.service.ts
+++ src/app/modules/claim/claim.service.ts
@@ -2,8 +2,49 @@ import { IDecodedUser } from "../../interfaces";
 import { prisma } from "../../shared/prisma";
 import { IClaim, IClaimStatus } from "./claim.interface";
 
-function findAllClaims() {
-    return prisma.claim.findMany()
+
+async function findAllClaims() {
+    return prisma.claim.findMany({
+        select: {
+            id: true,
+            userId: true,
+            foundItemId: true,
+            distinguishingFeatures: true,
+            lostDate: true,
+            status: true,
+            createdAt: true,
+            updatedAt: true,
+            foundItem: { 
+                select: {
+                    id: true,
+                    userId: true,
+                    categoryId: true,
+                    foundItemName: true,
+                    description: true,
+                    location: true,
+                    createdAt: true,
+                    updatedAt: true,
+                    user: {
+                        select: {
+                            id: true,
+                            name: true,
+                            email: true,
+                            createdAt: true,
+                            updatedAt: true
+                        }
+                    },
+                    category: { 
+                        select: {
+                            id: true,
+                            name: true,
+                            createdAt: true,
+                            updatedAt: true
+                        }
+                    }
+                }
+            }
+        }
+    });
 }
 
 function createNewClaim(payload: IClaim, user: IDecodedUser) {
