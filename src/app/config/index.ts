b21c863087e5e0ab9fe0135c42112e2002392277
Md. Mehedi Hasan Nabil <mehedihasannabil49@gmail.com>
2024-04-23 17:22:55 +0600

    done

--- src/app/config/index.ts
+++ src/app/config/index.ts
@@ -7,7 +7,7 @@ dotenv.config({
 
 export default {
     PORT: process.env.PORT || 5000,
-    DB_URI: process.env.DB_URI,
+    DATABASE_URL: process.env.DATABASE_URL,
     JWT_SECRET: process.env.JWT_SECRET,
     EXPIRES_IN: process.env.EXPIRES_IN
 }
