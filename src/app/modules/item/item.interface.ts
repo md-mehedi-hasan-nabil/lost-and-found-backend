d5bc1c3292456b458e1b25d51c77c14042908b57
Md. Mehedi Hasan Nabil <mehedihasannabil49@gmail.com>
2024-06-02 07:54:04 +0600

    item create

--- src/app/modules/item/item.interface.ts
+++ src/app/modules/item/item.interface.ts
@@ -5,6 +5,7 @@ export interface ICreateItem {
     description: string;
     itemType: ItemType;
     date: string;
+    time: string
     location: string;
     categoryId: string;
     image_url?: string;
--- src/app/modules/item/item.validator.ts
+++ src/app/modules/item/item.validator.ts
@@ -5,11 +5,10 @@ export const createItemValidationSchema = z.object({
     name: z.string({ required_error: "Name is required" }),
     description: z.string({ required_error: "Description is required" }),
     itemType: z.nativeEnum(ItemType, { required_error: "Item type is required" }),
-    date: z.string().refine((val) => !isNaN(Date.parse(val)), {
-        message: "Invalid date format",
-    }),
+    date: z.string({ required_error: "date is required" }),
+    time: z.string({ required_error: "time is required" }),
     location: z.string({ required_error: "Location is required" }),
-    categoryId: z.string().uuid("Invalid category ID format"),
+    categoryId: z.string({ required_error: "Category is required" }).uuid("Invalid category ID format"),
     image_url: z.string().url().optional(),
     contact: z.object({
         email: z.string(),
